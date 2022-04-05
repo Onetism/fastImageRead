f9ec16ee3191e36d1c0621c4f5a8930a66733cc1
onetism <onetism@163.com>
2022-04-05 12:04:52 +0800

    v1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ import sys
 from skbuild import setup
 
 setup(
-    name = "pyklb",
+    name = "imreadfast",
     version = "0.0.3.dev0",
     description = "Python wrapper of the KLB file format, a high-performance file format for up to 5-dimensional arrays.",
     long_description = "See https://bitbucket.org/fernandoamat/keller-lab-block-filetype",
     url = "https://github.com/bhoeckendorf/pyklb",
-    packages=["pyklb"],
+    packages=["imreadfast"],
     setup_requires = ["numpy"],
     install_requires = ["cython", "numpy"]
 )
